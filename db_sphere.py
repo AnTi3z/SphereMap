ff0c6b0686fe08593b7b16da17779bcf38fb85c0
AnTi3z <anti3z@anti3z.ru>
2020-07-09 19:01:56 +0300

    Clear almost all wrappers to ORM

--- db_sphere.py
+++ db_sphere.py
@@ -5,46 +5,27 @@ from db_models import *
 logger = logging.getLogger('SphereMap')
 
 
-def add_street(x, name):
-    Streets.insert(x=x, name=name).on_conflict_ignore().execute()
-    return get_street(name)
-
-
-def add_room(x, y):
-    Rooms.insert(x=x, y=y).on_conflict_ignore().execute()
-    return get_room(x, y)
-
-
-def add_passage(start, end):
-    return Passages.insert(start=start, end=end).on_conflict_ignore().execute()
-
-
 def check_description(desc):
     query = LocDescription.select()
     for row in query:
         if re.search(row.txt_regexp, desc):
             return row
 
-    row_id = LocDescription.insert(txt_regexp=desc, type=1).execute()
-    return LocDescription.get(LocDescription.id == row_id)
+    return LocDescription.create(txt_regexp=desc, type=1)
 
 
-def add_event(room, desc, user_id, date, entry):
-    event_id = Events.insert(timestamp=int(date.timestamp()), location=room, loc_desc=desc, user=user_id).execute()
-    if entry:
-        EntryPoints.insert(event=event_id).execute()
-
-
-def get_street(name):
-    return Streets.get_or_none(Streets.name == name)
-
-
-def get_room(x, y):
-    return Rooms.get_or_none((Rooms.x == x) & (Rooms.y == y))
+def erase_bandits(room_id):
+    # TODO: удалить бандитов
+    return 0  # число удаленных строк
 
 
 def get_last_room_stat(user_id):
     event = Events.select().join(Users).order_by(Events.timestamp.desc()).where(Users.tg_id == user_id).get()
+    return get_room_stat(event)
+
+
+def get_room_stat(event_id):
+    event = Events.get(Events.id == event_id)
     room = event.location
     query = (Events
              .select(Events, fn.COUNT(EventTypes.id).alias('cnt'))
--- mapper.py
+++ mapper.py
@@ -2,6 +2,8 @@ from telethon import TelegramClient, events
 import re
 import logging
 import db_sphere
+from db_models import *
+import mapper_bot
 from config import API_ID, API_HASH
 
 logger = logging.getLogger('SphereMap')
@@ -10,36 +12,40 @@ logging.basicConfig(format='[%(asctime)s.%(msecs)d] %(levelname)s:%(name)s:%(fun
                     level=logging.WARNING)
 logging.getLogger("SphereMap").setLevel(logging.WARNING)
 
-client = TelegramClient('AnTi3z client', API_ID, API_HASH)
+client = TelegramClient('AnTi3z', API_ID, API_HASH)
 
 bandit = False
 entry = False
 
 
-async def db_room_parse(room, user, date, entry_flag=False):
+def db_room_parse(room, user, date, entry_flag=False):
     logger.debug(f"ROOM: {room}")
     for passage in room["exits"]:
-        db_sphere.add_street(passage["x"], passage["street"])
-        db_sphere.add_room(passage["x"], passage["y"])
+        Streets.get_or_create(x=passage["x"], name=passage["street"])
+        Rooms.get_or_create(x=passage["x"], y=passage["y"])
 
-    street = db_sphere.get_street(room["street"])
+    street = Streets.get_or_none(Streets.name == room["street"])
     if street:
-        start_room = db_sphere.add_room(street.x, room["num"])
+        (current_room, _) = Rooms.get_or_create(x=street, y=room["num"])
         for passage in room["exits"]:
-            end_room = db_sphere.add_room(passage["x"], passage["y"])
-            db_sphere.add_passage(start_room, end_room)
+            (end_room, _) = Rooms.get_or_create(x=passage["x"], y=passage["y"])
+            Passages.get_or_create(start=current_room, end=end_room)
     else:
-        start_room = None
+        current_room = None
         logger.warning(f"No '{room['street']}' street found in DB.")
 
     desc = db_sphere.check_description(room["desc"])
-    if start_room:
-        return db_sphere.add_event(start_room, desc, user, date, entry_flag)
+    if current_room:
+        event = Events.create(timestamp=int(date.timestamp()), location=current_room, loc_desc=desc, user=user)
+        if entry_flag:
+            EntryPoints.create(event=event)
+        return event
 
 
 @client.on(events.NewMessage(chats=(944268265,), pattern=r"(?s)^Тебе удалось одолеть городского"))
 async def bandit_handler(event):
     global bandit
+    # TODO: Проверить, что это не гражданин
     bandit = True
 
 
@@ -76,9 +82,12 @@ async def town_handler(event):
                     "y": int(data.group(2))
                 })
     date = event.message.edit_date or event.message.date
-    await db_room_parse(room, event.message.to_id.user_id, date, entry_flag)
+    db_room_parse(room, event.message.to_id.user_id, date, entry_flag)
+    await client.inline_query('SpheriumMapperBot', "new_event")
 
 
-client.start()
-# client.add_event_handler(town_handler)
-client.run_until_disconnected()
+if __name__ == "__main__":
+    client.start()
+    mapper_bot.bot.start(bot_token=mapper_bot.BOT_TOKEN)
+    # client.add_event_handler(town_handler)
+    client.run_until_disconnected()
