5b321e37a3b106ccfd901bd2c047067d41aa2b5a
AnTi3z <anti3z@anti3z.ru>
2020-10-28 17:09:40 +0300

    Steal every 30 minutes

--- sphere/stealer.py
+++ sphere/stealer.py
@@ -31,48 +31,38 @@ class StealTimer:
     def set(self, delay):
         self.stop()
         global_state['task'] = Task.NONE
-        steal_list.clear()
         delay_gap = delay + 15
         self._task = asyncio.get_event_loop().call_later(delay_gap, self.set_steal)
         logger.info(f"Steal timer started for {delay_gap} sec")
 
 
-steal_list = []
 steal_timer = StealTimer()
 
 _ready_re = r"(?s)^🧙🏻‍♂️.+❤️\d+.+🛡\d+.+👊"
 _steal_re = r"(?s)^(?:Не найдя ничего лучше)|(?:Поискав подходящий случай)|(?:Побродив в округе)"
-_wait_re = r"(?s)^Тебе пока рано снова воровать.+через(?: (\d{1,2}) ч.)?(?: (\d{1,2}) м.)?(?: (\d{1,2}) с.)?"
+_wait_re = r"(?s)^Тебе пока рано снова воровать.+Через(?: (\d{1,2}) ч.)?(?: (\d{1,2}) м.)?(?: (\d{1,2}) с.)?"
 
 
 @events.register(events.MessageEdited(chats=(BOT_ID,), pattern=_ready_re))
 @events.register(events.NewMessage(chats=(BOT_ID,), pattern=_ready_re))
 async def ready_handler(event):
-    attempts_left = STEALER_CFG["attempts"] - len(steal_list)
-    if global_state['task'] == Task.STEALING and attempts_left > 0:
+    if global_state['task'] == Task.STEALING:
         time.sleep(random.uniform(1.1, 2.5))
         await event.message.respond("🔮 Сфериум")
-        for _ in range(attempts_left):
-            time.sleep(random.uniform(1.1, 2.5))
-            await event.message.respond("🦹🏼‍♂️ Воровство")
+        time.sleep(random.uniform(1.1, 2.5))
+        await event.message.respond("🦹🏼‍♂️ Воровство")
 
 
 @events.register(events.MessageEdited(chats=(BOT_ID,), pattern=_steal_re))
 @events.register(events.NewMessage(chats=(BOT_ID,), pattern=_steal_re))
 async def steal_handler(event):
-    logger.debug(f"New steal message with button: {event.reply_markup.rows[0].buttons[1].data.decode('utf-8')}")
-    steal_list.append((event.id, event.message.buttons[0][1].data.decode('utf-8')))
-    logger.debug(f"Steal list: {steal_list}")
-    if global_state['task'] == Task.STEALING and len(steal_list) >= STEALER_CFG["attempts"]:
-        msg_id, btn_data = random.choice(steal_list)
-        logger.info(f"Steal with btn_data: {btn_data}")
-        await event.client(
-            functions.messages.GetBotCallbackAnswerRequest(event.from_id,
-                                                           msg_id,
-                                                           data=btn_data.encode("utf-8"))
-        )
+    btn = event.message.buttons[0][1]
+    logger.debug(f"New steal message with button: {btn.data.decode('utf-8')}")
+    if global_state['task'] == Task.STEALING:
+        logger.info(f"Click button: {btn.data.decode('utf-8')}")
+        await btn.click()
 
-        steal_timer.set(7200)  # two hours
+        steal_timer.set(1800)  # half hours
 
         await asyncio.sleep(random.uniform(200, 250))  # Gap fo fighting
         time.sleep(random.uniform(1.1, 2.5))
@@ -92,7 +82,7 @@ async def wait_handler(event):
     if event.pattern_match.group(3):
         time_remain += int(event.pattern_match.group(3))  # seconds
 
-    steal_timer.set(time_remain)
+    steal_timer.set(1800)
 
 
 _steal_money_re = r"(?s)^Ты выкрал из кошелька бедолаги 💰(\d+)!$"
@@ -100,6 +90,7 @@ _steal_fight_re = r"(?s)^Кража не удалась, драка неизбе
 _steal_empty_re = r"(?s)^Увы, ничего украсть не удалось. Зато и не прилетело по башке.$"
 _steal_box_re = r"(?s)^Тебе удалось украсть (.+)!$"  # контейнер
 _steal_item_re = r"(?s)^Тебе удалось своровать (.+)!$"  # билет/ключ
+_steal_stone_re = r"(?s)^Кража не удалась.+Защитный камень"
 
 
 @events.register(events.NewMessage(chats=(BOT_ID,), pattern=_steal_money_re))
@@ -127,6 +118,11 @@ async def steal5_handler(event):
     logger.info(f"Steal jackpot: {event.pattern_match.group(1)}")
 
 
+@events.register(events.NewMessage(chats=(BOT_ID,), pattern=_steal_stone_re))
+async def steal6_handler(event):
+    logger.info(f"Failed steal(stone)")
+
+
 def activate(client, stealer_cfg):
     global STEALER_CFG
     STEALER_CFG = stealer_cfg
@@ -138,6 +134,7 @@ def activate(client, stealer_cfg):
     client.add_event_handler(steal3_handler)
     client.add_event_handler(steal4_handler)
     client.add_event_handler(steal5_handler)
+    client.add_event_handler(steal6_handler)
     logger.info("Stealer script activated")
 
 
