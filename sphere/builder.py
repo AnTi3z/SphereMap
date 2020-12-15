a34d3409c27db72209ee93f3efccacbf6f3fadc6
AnTi3z <anti3z@anti3z.ru>
2020-12-15 10:27:02 +0300

    Fix builder.py with new module system

--- sphere/builder.py
+++ sphere/builder.py
@@ -1,6 +1,7 @@
 import logging
 import random
 import time
+import itertools
 
 from telethon import events, errors
 
@@ -9,8 +10,7 @@ from .sphere import BOT_ID, global_state
 logger = logging.getLogger('Sphere.builder')
 logger.setLevel(logging.INFO)
 
-
-BUILDER_CFG = {}
+MODULE_CFG = {}
 
 _build_re = r'Ты решил отдохнуть, взять перекур и ушёл со стройки.'
 _buildings_re = r'Постройки фракции:'
@@ -18,40 +18,36 @@ _buildings_re = r'Постройки фракции:'
 
 @events.register(events.NewMessage(chats=(BOT_ID,), pattern=_build_re))
 async def go_to_buildings(event):
-    if BUILDER_CFG['enabled']:
-        time.sleep(random.uniform(1.1, 2.5))
-        await event.message.respond("/buildings")
+    time.sleep(random.uniform(1.1, 2.5))
+    await event.respond(r"/buildings")
 
 
 @events.register(events.NewMessage(chats=(BOT_ID,), pattern=_buildings_re))
 async def start_build(event):
-    if BUILDER_CFG['enabled']:
-        time.sleep(random.uniform(1.1, 2.5))
-        await try_click(event.message, 'frabuildbuild')
-        return
-
-
-async def try_click(msg, data):
     time.sleep(random.uniform(1.1, 2.5))
-    global_state['last_button'] = (msg.id, data)
+    for btn in itertools.chain.from_iterable(event.buttons):
+        if btn.data.decode() == 'frabuildbuild':
+            await try_click(btn)
+
+
+async def try_click(button):
+    time.sleep(random.uniform(1.1, 2.5))
+    global_state['last_button'] = button
     try:
-        await msg.click(data=data.encode('utf-8'))
+        await button.click()
         global_state['last_button'] = None
     except errors.BotResponseTimeoutError:
-        logger.warning(f"Button {data} answer timeout")
+        logger.warning(f"Button {button.data.decode()} answer timeout")
     except errors.MessageIdInvalidError:
-        logger.warning(f"Message with {data} was deleted")
+        logger.warning(f"Message with {button.data.decode()} was deleted")
 
 
-def activate(client, builder_cfg):
-    global BUILDER_CFG
-    BUILDER_CFG = builder_cfg
-    client.add_event_handler(go_to_buildings)
-    client.add_event_handler(start_build)
-    logger.info("Builder script activated")
+def activate():
+    logger.info("Sphere.Builder script activated")
 
 
-def deactivate(client):
-    client.remove_event_handler(go_to_buildings)
-    client.remove_event_handler(start_build)
-    logger.info("Builder script deactivated")
+def deactivate():
+    logger.info("Sphere.Builder script deactivated")
+
+
+HANDLERS = (go_to_buildings, start_build)
