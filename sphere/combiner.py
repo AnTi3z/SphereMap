deaa346e3982c2f1e6fc3d44766ca6f3ecae3e9e
AnTi3z <anti3z@anti3z.ru>
2020-12-17 13:03:48 +0300

    Remove redundant MODULE_CFG['enabled'] check

--- sphere/combiner.py
+++ sphere/combiner.py
@@ -17,16 +17,19 @@ _boxes_re = r'Коробочки:'
 
 @events.register(events.NewMessage(chats=(BOT_ID,), pattern=_boxes_re))
 async def combiner(event):
-    if MODULE_CFG['enabled']:
-        for level in range(MODULE_CFG['level']+1):
-            if search(fr'/union_{level}', event.text):
-                time.sleep(random.uniform(1.1, 2.5))
-                await event.respond(f"/union_conf_{level}")
-                break
+    for level in range(MODULE_CFG['level'] + 1):
+        if search(fr'/union_{level}', event.text):
+            time.sleep(random.uniform(1.1, 2.5))
+            await event.respond(f"/union_conf_{level}")
+            break
 
 
 def activate():
     logger.info("Sphere.Combiner script activated")
+
+
+def deactivate():
+    logger.info("Sphere.Combiner script deactivated")
 
 
 HANDLERS = (combiner,)
